14f1f40eaf1465872371ba0a033689c4661f7136
João Pedro Gonçalves <joao.kamun@gmail.com>
2018-02-19 18:13:22 -0300

    Changed box-shadows for Gocommerce

--- partials/_box-shadow.css.js
+++ partials/_box-shadow.css.js
@@ -1,7 +1,5 @@
 module.exports = `
-.shadow-1 { box-shadow: 0px 0px 4px 2px rgba( 0, 0, 0, 0.2 ); }
-.shadow-2 { box-shadow: 0px 0px 8px 2px rgba( 0, 0, 0, 0.2 ); }
-.shadow-3 { box-shadow: 2px 2px 4px 2px rgba( 0, 0, 0, 0.2 ); }
-.shadow-4 { box-shadow: 2px 2px 8px 0px rgba( 0, 0, 0, 0.2 ); }
-.shadow-5 { box-shadow: 4px 4px 8px 0px rgba( 0, 0, 0, 0.2 ); }
+.shadow-1 { box-shadow: 0px 0px 5px 0px rgba( 31, 46, 95, 0.08 ); }
+.shadow-2 { box-shadow: 0px 0px 10px 2px rgba( 31, 46, 95, 0.08 ); }
+.shadow-3 { box-shadow: -5px 0px 30px 0px rgba( 31, 46, 95, 0.08 ); }
 `
